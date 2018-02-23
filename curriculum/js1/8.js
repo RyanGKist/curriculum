6e6b867398c717b9a500aead19d58f08d0ec0cc7
Ryan Kistner <RyanGKistner@Patricks-MacBook-Pro-2.local>
2018-02-22 17:04:50 -0800

    finished 8.js

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,8 +5,15 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b,counter=0, denominator=0) => {
+  if(counter <= a){
+    if(a % counter == 0 && b % counter == 0){
+      denominator = counter;
+      return solution(a,b,counter+=1,denominator);
+    }
+    return solution(a,b,counter+=1,denominator);
+  }
+  return denominator;
 };
 
 module.exports = {
