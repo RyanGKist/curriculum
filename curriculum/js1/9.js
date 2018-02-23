391aa8a80aa4dd21b9a6a0e661b6e444f5f041ec
Ryan Kistner <RyanGKistner@Patricks-MacBook-Pro-2.local>
2018-02-22 17:23:39 -0800

    finished 9.js

--- curriculum/js1/9.js
+++ curriculum/js1/9.js
@@ -4,8 +4,12 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, counter=0) => {
+  a = a + 1;
+  if(a % 7 == 0){
+    return a;
+  }
+  return solution(a);
 };
 
 module.exports = {
